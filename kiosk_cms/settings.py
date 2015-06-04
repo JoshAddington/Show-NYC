cfc9ce706d7c9bb1a145e87102eb23c274ab4b0c
JoshAddington <josh@Control-Group-Employee-2.local>
2015-06-04 15:15:55 -0400

    add postgres creation script

--- kiosk_cms/settings.py
+++ kiosk_cms/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'kiosk_cms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgres_psycopg2',
+        'NAME': 'kiosk_cms',
+        'USER': 'intern',
+        'PASSWORD': 'cgrocks',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
---
+++ postgres.sh
@@ -0,0 +1,2 @@
+createdb kiosk_cms
+/Applications/Postgres.app/Contents/Versions/9.4/bin/psql -d postgres -c "create user intern with createrole superuser password '"'"'cgrocks'"'"';"
