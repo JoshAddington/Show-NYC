918355628e9c69c4f62532783bed45494472b19e
Emma Chesley <echesley@mit.edu>
2015-06-05 10:55:04 -0400

    Working on hiding and showing content

--- Website Branch/java.js
+++ Website Branch/java.js
@@ -11,7 +11,7 @@ function voteButton() {
 	//button text
 	var voteButton = document.getElementById('voteButton');
 	//make content conditionaly visible
-	if (displaySetting == 'none') {
+	if (displaySetting === 'none') {
 		//meaning the vote page is not visible
 		voteContent.style.display = 'block';
 		//change text on button
@@ -26,46 +26,66 @@ function voteButton() {
 
 function galleryButton() {
 	//get the content
-	galleryContent = document.getElementById("gallery");
+	galleryContent = document.getElementById("gallery"); 
 	//get the current value of the display
-	var gallerySetting = galleryContent.style.display;
+	var displaySetting = galleryContent.style.display;
+	//button text
+	var galleryButton = document.getElementById('galleryButton');
 	//make content conditionaly visible
-	if (displaySetting == 'none') {
+	if (displaySetting === 'none') {
 		//meaning the vote page is not visible
 		galleryContent.style.display = 'block';
+		//change text on button
+		galleryButton.innerHTML = 'HIDE GALLERY';
 	}
 	else {
 		galleryContent.style.display = 'none';
+		//change text on button
+		galleryButton.innerHTML = 'SHOW GALLERY';
 	}
 }
+
 
 function submitButton() {
 	//get the content
-	submitContent = document.getElementById("submit");
+	submitContent = document.getElementById("submit"); 
 	//get the current value of the display
 	var displaySetting = submitContent.style.display;
+	//button text
+	var submitButton = document.getElementById('submitButton');
 	//make content conditionaly visible
-	if (displaySetting == 'none') {
+	if (displaySetting === 'none') {
 		//meaning the vote page is not visible
 		submitContent.style.display = 'block';
+		//change text on button
+		submitButton.innerHTML = 'HIDE SUBMIT';
 	}
 	else {
 		submitContent.style.display = 'none';
+		//change text on button
+		submitButton.innerHTML = 'SHOW SUBMIT';
 	}
 }
 
+
 function aboutButton() {
 	//get the content
-	aboutContent = document.getElementById("about");
+	aboutContent = document.getElementById("about"); 
 	//get the current value of the display
 	var displaySetting = aboutContent.style.display;
+	//button text
+	var aboutButton = document.getElementById('aboutButton');
 	//make content conditionaly visible
-	if (displaySetting == 'none') {
+	if (displaySetting === 'none') {
 		//meaning the vote page is not visible
 		aboutContent.style.display = 'block';
+		//change text on button
+		aboutButton.innerHTML = 'HIDE ABOUT';
 	}
 	else {
 		aboutContent.style.display = 'none';
+		//change text on button
+		aboutButton.innerHTML = 'SHOW ABOUT';
 	}
 }
 
