76ea1e1103ababc3cbad390bb7bfc462f6ece309
JoshAddington <josh@Control-Group-Employee-2.local>
2015-06-10 17:58:56 -0400

    REST API: Images, TODO API auth

--- kiosk_cms/images/api.py
+++ kiosk_cms/images/api.py
@@ -1,5 +1,6 @@
 from restless.dj import DjangoResource
 from restless.preparers import FieldsPreparer
+from campaigns.api import CampaignResource
 
 from .models import Image
 
@@ -10,15 +11,41 @@ class ImageResource(DjangoResource):
 		'author': 'user_id.username',
 		'url': 'image.url',
 		'score': 'score',
-		# 'campaign': 'campaign_id',
+		'campaign': 'campaign_id.name',
+		'campaign_id': 'campaign_id.id',
 		'campaign_winner': 'campaign_winner',
 		'flagged': 'flagged',
 	})
 
 	# GET /api/images/
 	def list(self):
-		return Image.objects.all()
+		return Image.objects.filter(flagged=False)
 
 	# GET /api/images/<pk>/
 	def detail(self, pk):
-		return Image.objects.get(id=pk)
+		return Image.objects.select_related().get(pk=pk)
+
+	# POST /api/images/
+	def create(self):
+		return Image.objects.create(
+			image=self.data['title'],
+			user_id=User.objects.get(username=self.data['author']),
+		    campaign_id = Campaign.objects.get(id=self.data['campaign_id']),
+		)
+
+	# PUT /api/images/<pk>/
+	def update(self, pk):
+		try:
+			image = Image.objects.get(pk=pk)
+		except Image.DoesNotExist:
+			image = Image()
+
+		image.title = self.data['title']
+		image.user_id = User.objects.get(username=self.data['author'])
+		image.campaign_id = Campaign.objects.get(id=self.data['campaign_id'])
+		image.save()
+		return image
+
+	# DELETE /api/images/<pk>/
+	def delete(self, pk):
+		Image.objects.get(pk=pk).delete()
