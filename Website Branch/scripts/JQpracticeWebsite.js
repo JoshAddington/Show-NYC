9d3b2438c46732e107703e01255ae67c84878462
Omayeli Arenyeka <oa495@nyu.edu>
2015-06-12 17:38:52 -0400

    menu stuff
    - white menu icon in header
    - menu disappears when you click on link

--- Website Branch/scripts/JQpracticeWebsite.js
+++ Website Branch/scripts/JQpracticeWebsite.js
@@ -1,16 +1,4 @@
 $(document).ready(function() {
-  
-  $('.tabs').mouseenter(function() {
-    $(this).animate({
-      height: '+=0px'
-    });
-  });
-  $('.tabs').mouseleave(function() {
-    $(this).animate({
-      height: '-=0px'
-    }); 
-  });
-
   $('#voteButton').click(function() {
     var condition = true;
     if (condition === true){
@@ -51,31 +39,23 @@ $(document).ready(function() {
       }
    }); 
 
-var main = function() {
-//Push the body and the nav over by 285px over 
-  $('.icon-menu').click(function() {
-    $('.menu').animate({
-      left: "0px"
-    }, 200);
+  
+    $('.icon-menu').click(function() {
+      $('.menu').animate({
+        left: "0px"
+      }, 200);
+      $('.menu').css('left', '-300px');
+     });
 
-    $('body').animate({
-      left: "350px"
-    }, 200);
-  });
 
-//Then push them back 
-  $('.icon-close').click(function() {
-    $('.menu').animate({
-      left: "-350px"
-    }, 200);
+      $('.icon-close').click(function() {
+      $('.menu').animate({
+        left: "-350px"
+      }, 200);
 
-    $('body').animate({
-      left: "-350px"
-    }, 200);
-  });
-//push back after clilcking elsewhere
-}
-
-$(document).ready(main);
+      $('body').animate({
+        left: "-350px"
+      }, 200);
+    });
 
 });
