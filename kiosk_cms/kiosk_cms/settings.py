30cd409a038903ab16886396d9b4619dab522828
JoshAddington <josh.addington@gmail.com>
2015-06-13 15:56:10 -0400

    adds collectstatic command to .ebextension

--- kiosk_cms/kiosk_cms/settings.py
+++ kiosk_cms/kiosk_cms/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'www', 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
