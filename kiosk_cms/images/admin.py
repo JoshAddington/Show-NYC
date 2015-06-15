34ad7acf4574149ee6bedaee2c83a13a61d52015
JoshAddington <josh.addington@gmail.com>
2015-06-15 14:01:50 -0400

    adds active field to Image model and Image admin page

--- kiosk_cms/images/admin.py
+++ kiosk_cms/images/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import Image
 
 class ImageAdmin(admin.ModelAdmin):
-	list_display = ('thumb','score', 'flagged', 'campaign_id', 'user_id')
+	list_display = ('thumb','score', 'flagged', 'active', 'campaign_id', 'user_id')
+	
+	# Make the active column editable from the list display
+	list_editable = ('active', )
+
 	# set the thumbnail to read only
 	readonly_fields = ('thumb', )
 
