9c1722fb6752cf4e669d895f6957b6e536195876
JoshAddington <josh.addington@gmail.com>
2015-06-15 14:06:01 -0400

    invert flagged column icon in admin, to more accurately reflect perception of setting.

--- kiosk_cms/images/admin.py
+++ kiosk_cms/images/admin.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 from .models import Image
 
 class ImageAdmin(admin.ModelAdmin):
-	list_display = ('thumb','score', 'flagged', 'active', 'campaign_id', 'user_id')
+	list_display = ('thumb','score', 'flagged_col', 'active', 'campaign_id', 'user_id')
 	
 	# Make the active column editable from the list display
 	list_editable = ('active', )
 
 	# set the thumbnail to read only
 	readonly_fields = ('thumb', )
+
+	def flagged_col(self, obj):
+		return not obj.flagged # invert the boolean value
+	flagged_col.boolean = True
+	flagged_col.admin_order_field = 'flagged'
+	flagged_col.short_description  = 'Flagged'
 
 
 # Register your models here.
