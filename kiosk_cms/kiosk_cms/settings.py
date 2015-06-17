2e5dbfda17b407ec59d18fdd036cfee63c68c0bb
JoshAddington <josh.addington@gmail.com>
2015-06-17 12:07:06 -0400

    add S3 hookup settings

--- kiosk_cms/kiosk_cms/settings.py
+++ kiosk_cms/kiosk_cms/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS += (
     'apis',
     'debug_toolbar',
     'restless',
+    'storages',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -136,6 +137,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, '..', 'www', 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+# Amazon Web Services settings     
+AWS_STORAGE_BUCKET_NAME = os.environ['BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# S3 URL
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# Media Files settings
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'kiosk_cms.custom_storages.MediaStorage' 
