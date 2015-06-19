5d835741ebbf17ddfaa1e98489e3af652adcdbc7
JoshAddington <josh.addington@gmail.com>
2015-06-19 13:58:10 -0400

    adds /api/campaigns/active/ endpoint for getting campaigns that are currently active

--- kiosk_cms/campaigns/views.py
+++ kiosk_cms/campaigns/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from rest_framework import status
@@ -6,13 +8,19 @@ from rest_framework.response import Response
 from .models import Campaign
 from .serializers import CampaignSerializer
 
-def index(request):
-    return render(request, 'index.html')
 
 @api_view(['GET'])
 def campaign_collection(request):
 	if request.method == 'GET':
 		campaigns = Campaign.objects.all().select_related()
+		serializer = CampaignSerializer(campaigns, many=True)
+		return Response(serializer.data)
+
+@api_view(['GET'])
+def active_campaign_collection(request):
+	if request.method == 'GET':
+		current_time = datetime.datetime.now()
+		campaigns = Campaign.objects.filter(start_date__lte=current_time).filter(end_date__gte=current_time)
 		serializer = CampaignSerializer(campaigns, many=True)
 		return Response(serializer.data)
 	
