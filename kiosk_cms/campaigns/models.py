1763c2a6a375b618c72db489c96f4357b2eae383
JoshAddington <josh.addington@gmail.com>
2015-06-23 13:02:01 -0400

    adds description to Campaigns model, removes some old code from vote template

--- kiosk_cms/campaigns/models.py
+++ kiosk_cms/campaigns/models.py
@@ -6,6 +6,7 @@ class Campaign(models.Model):
     sponsor = models.ForeignKey(User, related_name='campaigns', limit_choices_to={'is_staff': True})
     name = models.CharField(max_length=100)
     slug = models.SlugField(blank=True)
+    description = models.TextField()
     start_date = models.DateTimeField(blank=True)
     end_date = models.DateTimeField(blank=True)
 
