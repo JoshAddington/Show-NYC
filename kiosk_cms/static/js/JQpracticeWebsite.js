48f8bacd1db76a20a7d8fe6f181ff4233da03b63
Emma Chesley <emmachesley@controlgroup.com>
2015-06-29 13:48:06 -0400

    Call to action submit banner

--- kiosk_cms/static/js/JQpracticeWebsite.js
+++ kiosk_cms/static/js/JQpracticeWebsite.js
@@ -33,6 +33,17 @@ $(document).ready(function() {
       $('#aboutButton').removeClass("highlight")}
   }); 
 
+  //Submit 
+  $('#submit-ribbon').click(function(){
+    var condition = true;
+    if (condition === true){
+      $('#filter-buttons').hide();
+      $('#voteButton').removeClass("highlight")
+      $('#galleryButton').removeClass("highlight")
+      $('#submitButton').addClass("highlight")
+      $('#aboutButton').removeClass("highlight")}
+  });
+
    //About Button Display
   $('#aboutButton').click(function() {
     var condition = true;
