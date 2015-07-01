83d13173d2397ede8e33b15b810d213164161112
Sage McGill <sage@Control-Group-Employee-2.local>
2015-07-01 14:14:03 -0400

    vote changes

--- kiosk_cms/static/js/angular/app/controllers.js
+++ kiosk_cms/static/js/angular/app/controllers.js
@@ -9,7 +9,7 @@ angular.module('myApp.controllers', [])
   $scope.upvote = function(id) {
     $http.get('http://intern-cms-dev.elasticbeanstalk.com/api/images/'+id+'/upvote/').
       success(function(data, status, headers, config) {
-        // $scope.load()
+        console.log(id)
       }).
       error(function(data, status, headers, config) {
       });
@@ -28,6 +28,7 @@ angular.module('myApp.controllers', [])
     // console.log($sco.isClicked)
     console.log(document.getElementById("emptyHeart"+id))
     document.getElementById("emptyHeart"+id).src = 'static/icons/FullHeartRed.png'
+    // document.getElementById("emptyHeart"+id).id = 'fullHeart'
     // $scope.isClicked= true
     // return false
 
