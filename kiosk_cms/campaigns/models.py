e78fdd3a7ab268c0110728eba6ebe149dc741c5c
JoshAddington <josh.addington@gmail.com>
2015-07-06 17:48:07 -0400

    removes make_default from campaign model

--- kiosk_cms/campaigns/models.py
+++ kiosk_cms/campaigns/models.py
@@ -24,22 +24,6 @@ class Campaign(models.Model):
     def is_active(self):
         return self.active
 
-    # sets a campaign as the default campaign if a default campaign has not
-    # been set. If successful, sets campaign to active if an active
-    # campaign does not exist.
-    def make_default(self):
-        try:
-            default_campaign = Campaign.objects.get(default_campaign=True)
-            if default_campaign:
-                return "Default campaign already set to %s" % default_campaign.name
-            else:
-                self.default = True
-                active = Campaign.objects.get(active=True)
-                if not active:
-                    self.active = True
-        except Exception, e:
-            raise e
-
     def activate(self):
         old_campaign = Campaign.objects.get(active=True)
         old_campaign.active = False
