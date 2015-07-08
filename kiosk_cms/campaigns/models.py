c1a040b2c731fa5e8a8345cb9a9c4b2df34a71de
JoshAddington <josh.addington@gmail.com>
2015-07-08 12:09:18 -0400

    formatting

--- kiosk_cms/campaigns/models.py
+++ kiosk_cms/campaigns/models.py
@@ -6,7 +6,11 @@ from django.utils import timezone
 
 # Create your models here.
 class Campaign(models.Model):
-    sponsor = models.ForeignKey(User, related_name='campaigns', limit_choices_to={'is_staff': True})
+    sponsor = models.ForeignKey(
+        User,
+        related_name='campaigns',
+        limit_choices_to={'is_staff': True}
+    )
     name = models.CharField(max_length=100)
     slug = models.SlugField(blank=True)
     description = models.TextField(default="Campaign")
