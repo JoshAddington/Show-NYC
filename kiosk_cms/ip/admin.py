5e72934ddb5f906c55b068a9a1a6018affea8d6a
JoshAddington <josh.addington@gmail.com>
2015-07-08 13:45:56 -0400

    makes ip address the title of entries in IP admin page

--- kiosk_cms/ip/admin.py
+++ kiosk_cms/ip/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import IP
 
+
+class IPAdmin(admin.ModelAdmin):
+	list_display = ('ip_address',)
+
+
 # Register your models here.
-admin.site.register(IP)
+admin.site.register(IP, IPAdmin)
